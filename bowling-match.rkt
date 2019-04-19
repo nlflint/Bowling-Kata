9e537fbabd187a8d5b950ca95532478ef43729f6
Nathan Flint <nlflint@gmail.com>
2019-04-19 16:01:10 -0700

    Cleaned up solution

--- bowling-match.rkt
+++ bowling-match.rkt
@@ -2,19 +2,23 @@
 
 (require rackunit)
 
-(define (strike? roll) (eq? 10 roll))
-(define (spare? rolls) (eq? 10 (apply + rolls)))
 (define (last-frame? rolls) (< (length rolls) 4))
 
 (define (score rolls)
-  (let ([first (car rolls)]
-         [second (cadr rolls)]
-         [third (lambda () (caddr rolls))])
-  (cond
-    [(last-frame? rolls) (apply + rolls)]
-    [(strike? first) (+ 10 second (third) (score (cdr rolls)))]
-    [(spare? (list first second)) (+ 10 (third) (score (cddr rolls)))]
-    [else (+ first second (score (cddr rolls)))])))
+  (match rolls
+    [(list rest ...) #:when (last-frame? rest)
+     (apply + rest)]
+    
+    [(list 10 next next-after rest ...)
+     (+ 10 next next-after (score (cons next-after rest)))]
+    
+    [(list this next next-after _ ...) #:when (= 10 (+ this next))
+     (+ 10 next-after (score (cddr rolls)))]
+    
+    [(list this next _ ... )
+     (+ this next (score (cddr rolls)))]))
+
+
 
 ; tests
 (define (score-many count digit)
@@ -26,4 +30,5 @@
 (check-eq? (score-many 21 5) 150 "all spares")
 (check-eq? (score-many 12 10) 300 "perfect game")
 (check-eq? (score `(10 5 5 4 1 8 2 10 10 5 4 7 3 5 4 7 3 2 )) 148 "Realistic game")
+
 
